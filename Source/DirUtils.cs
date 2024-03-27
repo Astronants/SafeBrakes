cca8180b48c0f8d499677a5d46ac28723a48eab6
Astronants <40693768+Astronants@users.noreply.github.com>
2024-03-27 21:22:29 +0100

    adding directory utils

---
+++ Source/DirUtils.cs
@@ -0,0 +1,12 @@
+﻿using System.IO;
+
+namespace SafeBrakes
+{
+    internal class DirUtils
+    {
+        internal static readonly string GameDataDir = UrlDir.CreateApplicationPath("GameData");
+        internal static readonly string ModDir = Path.Combine(GameDataDir, "SafeBrakes");
+        internal static readonly string AppIconsDir = Path.Combine(ModDir, "Textures");
+        internal static readonly string PresetsDir = Path.Combine(ModDir, "Settings");
+    }
+}
